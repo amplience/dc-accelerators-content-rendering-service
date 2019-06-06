a4176227673ccc1895d0182cfbafd6f6437be4cf
Darren Lee <dlee@amplience.com>
2019-06-06 10:59:20 +0100

    feat(card): refactor card component for developer workflow

---
+++ src/card/card.stories.js
@@ -0,0 +1,52 @@
+import {
+  storiesOf
+} from '@storybook/html';
+
+import {
+  renderContent
+} from '../../.storybook/rendering-service';
+
+import cardStyles from './card.scss';
+
+const sampleContent = {
+  "@id": "http://content.cms.amplience.com/29bd9901-691f-4f35-adb2-ad93c85cd01b",
+  "_meta": {
+    "schema": "https://dev-solutions.s3.amazonaws.com/DynamicContentTypes/Accelerators/card.json",
+    "name": "accelerator-card-1"
+  },
+  "cardImage": {
+    "@id": "http://content.cms.amplience.com/ece6b9f2-b58a-4420-937d-a05e9970a459",
+    "@type": "https://dev-solutions.s3.amazonaws.com/DynamicContentTypes/Accelerators/image.json",
+    "_meta": {
+      "schema": "https://dev-solutions.s3.amazonaws.com/DynamicContentTypes/Accelerators/image.json",
+      "name": "accelerator-image-card-1"
+    },
+    "image": {
+      "@id": "http://image.cms.amplience.com/293fae56-2d8f-459e-b375-b65e524d3c69",
+      "_meta": {
+        "schema": "http://bigcontent.io/cms/schema/v1/core#/definitions/image-link"
+      },
+      "id": "293fae56-2d8f-459e-b375-b65e524d3c69",
+      "name": "pexels-photo-128939",
+      "endpoint": "csdemo",
+      "defaultHost": "i1.adis.ws",
+      "mediaType": "image"
+    },
+    "roundel": [],
+    "imageAltText": "alt txt",
+    "seoText": "seo txt"
+  },
+  "cardName": "Card 1",
+  "link": {
+    "value": "http://google.com",
+    "_meta": {
+      "schema": "https://dev-solutions.s3.amazonaws.com/DynamicContentTypes/Accelerators/link.json"
+    },
+    "label": "link card 1"
+  },
+  "description": "Card 1 description",
+  "@type": "https://dev-solutions.s3.amazonaws.com/DynamicContentTypes/Accelerators/card.json"
+};
+
+storiesOf('Card', module)
+  .add('Example content', () => renderContent('acc-template-card', sampleContent));
